1a8a08b77b49655c01800e93adbc99317f071aed
unknown <faridun.nk@gmail.com>
2021-08-15 17:52:07 +0500

    BankApp

---
+++ BankApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Процент: ");
+            float P = float.Parse(Console.ReadLine());
+            int K = 1;
+            float S = 1000;
+            while (S <= 1100) {
+                ++K;
+                S += S * P / 100;
+            }
+            Console.WriteLine($"Количество месяцев: {K}\nИтоговый размер вклада: {Math.Round(S,2)}");
+        }
+    }
+}
